645cd8f3211bd4063c50365e289bcf61f1209469
Suyog Acharya <asuyog042@gmail.com>
2024-05-06 08:17:00 +0545

    team v team api done

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
 from flask import Flask, request, jsonify
+import ipl
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return 'Hello World!!'
+
+@app.route('/api/teams')
+def teams():
+    teams=ipl.teamsapi()
+    return jsonify(teams)
+
+@app.route('/api/teamvteam')
+def teamvteam():
+    team1=request.args.get('team1')
+    team2=request.args.get('team2')
+    response=ipl.teamsvsteams(team1, team2)
+    return jsonify(response)
+
 
 app.run(port=5000, debug=True)
--- ipl.py
+++ ipl.py
@@ -5,6 +5,37 @@ import pandas as pd
 ipl_matches= "https://docs.google.com/spreadsheets/d/e/2PACX-1vTgCcxYBcqxUAuYHgrqUPsdXpausK3AF-B712JJaGHHA6BKtN3LCuoiJ1VQakm_umUU1nNkxy2K79QA/pub?output=csv"
 matches=pd.read_csv(ipl_matches)
 
-print(matches.head(5))
+#print(matches.head(5))
+
+def teamsapi():
+    teams= list(matches.Team1.unique())
+    teams_dict= {
+        "teams": teams
+    }   
+    return teams_dict
+
+def teamsvsteams(team1, team2):
+    valid_teams= list(matches.Team1.unique())
+    if team1 and team2 in valid_teams:
+        temp_df=matches[(matches['Team1']==team1) & (matches['Team2']==team2) | (matches['Team1']==team2) & (matches['Team2']==team1)]
+        total_matches=temp_df.shape[0]
+        matches_wonby_team1=int(temp_df['WinningTeam'].value_counts()[team1])
+        matches_wonby_team2=int(temp_df['WinningTeam'].value_counts()[team2])
+
+        draws=total_matches-matches_wonby_team1-matches_wonby_team2
+
+        response={
+            "team1": team1,
+            "team2": team2,
+            "total_matches": total_matches,
+            "team1_wins": matches_wonby_team1,
+            "team2_wins": matches_wonby_team2,
+            "draws": draws
+        }
+        return response
+    else:
+        return {"message": "Invalid team names"}
+
+      
 
 
